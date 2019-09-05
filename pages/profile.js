b06efe772511aabffb4231ac1c7ccf5b1f58fc2a
originalzeek <54872090+originalzeek@users.noreply.github.com>
2019-09-05 01:40:44 -0400

    Update profile.js
    Added Welcome Admital and Background Image

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,17 @@
 import Header from '../reactComponents/Header'
 
-export default () => 
+export default () =>
+
 
 <div>
     <Header />
+    <div>
+        <h1><font color="#FFE81F">Welcome Admiral [User]</font></h1>
+    </div>
+    
+
 </div>
+
+document.body.style.backgroundImage="url(https://assetstorev1-prd-cdn.unity3d.com/package-screenshot/84608b7c-f965-4648-b208-744cef2c53bc_scaled.jpg"
+
+
