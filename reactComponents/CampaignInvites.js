31ed6a944c2d307763eb94d51324cfd1e05522f5
danbuis <dan.buis88@gmail.com>
2019-11-15 12:48:59 -0500

    Changed invited player text from the userID to the username

--- reactComponents/CampaignInvites.js
+++ reactComponents/CampaignInvites.js
@@ -1,14 +1,43 @@
 import CampaignInvitesNew from '../reactComponents/CampaignInvitesNew'
+import axios from 'axios';
 
 class CampaignInvites extends React.Component {
+    state = {
+        users:[]
+    }
+
+    constructor(props){
+        super(props)
+        console.log("about to populate the state")
+        this.populateState()
+    }
+
+    async populateState(){
+        const getUserData = async invite =>{
+            console.log("looking up user")
+            const user = await axios.get('http://localhost:3000/user/'+invite.userID);
+            const userdata = await user.data
+            await console.log(userdata)
+            return await userdata
+          }
+        
+        const users = await Promise.all(this.props.invites.map(invite => getUserData(invite)))
+        
+        await this.setState({
+            users: users
+        })
+
+        await console.log(this.state.users)
+    }
+
     populateTable(){
-        if(this.props.invites.length===0 ){
+        if(this.props.invites.length===0 || this.state.users.length === 0){
             return (<p> No pending invites</p>)
         }else{
             const rows = this.props.invites.map((invite, index) => {
                 return (
                     <tr key={index}>
-                        <td>{invite.userID}</td>
+                        <td>{this.state.users[index].username}</td>
                         <td>{invite.faction}</td>
                     </tr>
                     )
