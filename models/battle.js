4c3cf3d68ca30b1bbc962ea2659b587f061b9160
danbuis <dan.buis88@gmail.com>
2019-12-03 11:43:28 -0500

    Basic Battle Schema added

---
+++ models/battle.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+
+var battleSchema = mongoose.Schema({
+  attackingCommander: String,
+  defendingCommander: String,
+  attackingFaction: String,
+  defendingFaction: String,
+  System: String,
+  currentOwnership: String,
+  winner: {String, default:""},
+  loser: {String, default:""}
+});
+
+battleSchema.methods.resolveBattle = function(winningFaction){
+  if(winningFaction === attackingFaction){
+    this.winner = this.attackingCommander
+    this.loser = this.defendingCommander
+  }else{
+    this.loser = this.attackingCommander
+    this.winner = this.defendingCommander
+  }
+}
+
+
+var Battle = mongoose.model("Battle", battleSchema);
+
+module.exports = Battle;
