9edc951839cf0caab99556088fe1e0021432f448
danbuis <dan.buis88@gmail.com>
2019-12-09 16:47:49 -0500

    minor cleaning to server

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.prepare().then(() => {
     server.post("/signup", function(req, res, next){
         var username = req.body.username;
         var password = req.body.password;
-        var lowercaseUser = username.toLowerCase().replace(/[\s]/gi,"&nbsp")
+        var lowercaseUser = username.toLowerCase().replace(/[\s]/gi,"&nbsp;")
       
         User.findOne({ username: lowercaseUser }, function(err, user) {
       
@@ -100,7 +100,7 @@ app.prepare().then(() => {
         var playerCount = req.body.players;
         var faction = req.body.faction;
 
-        var campaignName = preprocessedCampaignName.replace(/[\s]/gi,"&nbsp")
+        var campaignName = preprocessedCampaignName.replace(/[\s]/gi,"&nbsp;")
 
         const campaign = await Campaign.find({name:campaignName})
         if(campaign.length>0){
@@ -221,7 +221,7 @@ app.prepare().then(() => {
     server.post("/changeCommanderName", async function(req, res, next){
       const commander = await Commander.findById(req.body.commanderID)
       const newName=req.body.newName
-      const cleanName = newName.toLowerCase().trim().replace(/[\s]/gi,"&nbsp");
+      const cleanName = newName.toLowerCase().trim().replace(/[\s]/gi,"&nbsp;");
 
       if(cleanName === "none"){
         res.redirect("/error/2")
@@ -408,8 +408,6 @@ app.prepare().then(() => {
     })
 
     server.post("/battleResolve", async function(req, res, next){
-      console.log("body")
-      console.log(req.body)
       const battle = await Battle.findById(req.body.battle)
       const campaign = await Campaign.findOne({name:req.body.campaign})
 
@@ -430,10 +428,8 @@ app.prepare().then(() => {
           resolved = resolved+1
         }      
       }
-      await console.log(resolved)
       if(resolved == await battleList.length){allResolved = true}
 
-      console.log("RESOLVED :"+allResolved)
       if(await allResolved){
         await campaign.changeRound(campaign.round+1)
       }
